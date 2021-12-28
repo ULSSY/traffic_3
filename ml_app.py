a674c737515968d7944f4d7385d361f270f44ec4
ULSSY <96038813+ULSSY@users.noreply.github.com>
2021-12-28 18:27:35 +0900

    2021-12-28

--- ml_app.py
+++ ml_app.py
@@ -10,15 +10,16 @@ import altair as alt
 
 
 
+
 def run_ml_app() :
     st.sidebar.header('<목록>')
     st.sidebar.subheader('1)상관계수')
     st.sidebar.subheader('2)특정지역 자료분석')  
-
+   
   
     df=pd.read_csv('data/도로교통.csv',encoding='CP949')
     st.subheader('상관계수')
-    sb.set(font="Malgun Gothic", rc={"axes.unicode_minus":False}, style='darkgrid') 
+    sb.set(font="Malgun Gothic", rc={"axes.unicode_minus":False}, style='darkgrid')
     sb.scatterplot(data=df)
     
     df_corr=df.iloc[:,3:]
